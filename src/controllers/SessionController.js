dd48f5ef53865f51cc6162ba713ab8f290e1f2f0
Lucas Bastos <lucasbastos@gmail.com>
2020-03-25 17:50:31 -0300

    adicionando Procfile

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -10,6 +10,7 @@ module.exports = {
     if (!ong) {
       return res.status(400).json({ error: "No ong found with this ID" });
     }
+    req.headers.authorization = id;
     return res.status(200).json({ message: `Bem vindo ${ong.name}` });
   }
 };
